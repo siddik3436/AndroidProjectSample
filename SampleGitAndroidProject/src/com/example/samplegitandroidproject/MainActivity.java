6e3cb41093a8b63bb3b1388c10d10a1988dbbe4b
siddik3436 <siddik3436@gmail.com>
2015-03-20 16:54:44 +0600

    Add Activity

--- SampleGitAndroidProject/src/com/example/samplegitandroidproject/MainActivity.java
+++ SampleGitAndroidProject/src/com/example/samplegitandroidproject/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.samplegitandroidproject;
 
+
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -12,4 +15,12 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+ public void onClick(View view){
+    	
+    	Intent intent= new Intent(this, SecondActivity.class);
+    	intent.putExtra("URL", "http://www.vogella.com");
+    	startActivity(intent);
+    	
+    }
+
 }
